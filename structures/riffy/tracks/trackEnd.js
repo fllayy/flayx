c6065a72f19a778ddae7f8a08864eb2962a267f3
fllay <fllay@PCFFLAY>
2026-03-16 18:58:21 +0100

    security to test

--- structures/riffy/tracks/trackEnd.js
+++ structures/riffy/tracks/trackEnd.js
@@ -4,4 +4,14 @@ client.riffy.on('trackEnd', async (player) => {
     if (!player) return;
 
     if (player.message) await player.message.delete();
+
+    const voiceChannel = client.channels.cache.get(player.voiceChannel);
+    if (voiceChannel) {
+        const humanCount = voiceChannel.members.filter(m => !m.user.bot).size;
+        if (humanCount === 0) {
+            const textChannel = client.channels.cache.get(player.textChannel);
+            if (textChannel) textChannel.send('📭 No one is left in the voice channel, leaving.');
+            player.destroy();
+        }
+    }
 })
