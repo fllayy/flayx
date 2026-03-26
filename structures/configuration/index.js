c21eb5d82079ad5b2885be328feed0aa2f21f092
fllayy <74819714+fllayy@users.noreply.github.com>
2026-03-26 11:14:55 +0100

    Change sharding condition to false

--- structures/configuration/index.js
+++ structures/configuration/index.js
@@ -2,13 +2,13 @@ module.exports = {
     client_token: process.env.CLIENT_TOKEN,
     client_id: process.env.CLIENT_ID,
     developers: process.env.DEVELOPERS ? process.env.DEVELOPERS.split(",") : [],
-    sharding: process.env.SHARDING === "true",
+    sharding: process.env.SHARDING === "false",
     nodes: [
         {
             host: process.env.LAVALINK_HOST || "lavalink",
             port: parseInt(process.env.LAVALINK_PORT) || 2333,
             password: process.env.LAVALINK_PASSWORD || "youshallnotpass",
-            secure: process.env.LAVALINK_SECURE === "true"
+            secure: process.env.LAVALINK_SECURE === "false"
         }
     ]
 }
